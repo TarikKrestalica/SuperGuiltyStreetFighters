a60976e36c7c558499101232b4d753edd8138744
TarikKrestalica <krestalicatarik@gmail.com>
2024-03-29 01:06:56 -0400

    Move Manager script, check for valid combo testing, need animations

--- SuperGuiltyStreetKombat/Assets/Scripts/Player.cs
+++ SuperGuiltyStreetKombat/Assets/Scripts/Player.cs
@@ -6,11 +6,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] string moveCombo;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Reading input from keyboard: https://docs.unity3d.com/ScriptReference/Input.html
     void Update()
@@ -24,10 +19,16 @@ public class Player : MonoBehaviour
         }
 
         moveCombo += Input.inputString;
+        if (!GameManager.moveManager.isThereComboMove(moveCombo))
+        {
+            return;
+        }
+
+        PerformMove(moveCombo);
+        moveCombo = "";
     }
 
-    // Keep Track of Input as the player performs the combo move
-
+    // Keep track of input as the player performs the combo move
     int GetKeyPressed(string input)
     {
         if(!int.TryParse(input, out int number))
@@ -35,30 +36,26 @@ public class Player : MonoBehaviour
             return -1;
         }
 
+        // Link number to right number
         switch (number)
         {
-            case 1:
-                return 1;
-            case 2:
-                return 2;
-            case 3:
-                return 3;
-            case 4:
-                return 4;
-            case 5:
-                return 5;
-            case 6:
-                return 6;
-            case 7:
-                return 7;
-            case 8:
-                return 8;
-            case 9:
-                return 9;
-            case 0:
-                return 0;
-            default:
-                return -1;
+            case 1: return 1;
+            case 2: return 2;
+            case 3: return 3;
+            case 4: return 4;
+            case 5: return 5;
+            case 6: return 6;
+            case 7: return 7;
+            case 8: return 8;
+            case 9: return 9;
+            case 0: return 0;
+            default: return -1;
         }
+    }
+
+    void PerformMove(string literal)
+    {
+        CombatMove move = GameManager.moveManager.GetCombatMove(literal);
+        Debug.Log("Move performed is: " + move);
     }
 }
